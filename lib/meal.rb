5097ebd3ae7938afca4e79ddf83f5048e72e3e63
Evan <evanfujita@gmail.com>
2020-12-14 08:12:52 -0600

    commit tree 0c92145eef73f87fc0a9e842770a399f511b021c

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,22 @@
+require 'pry'
+require_relative 'waiter.rb'
+require_relative 'customer.rb'
+
 class Meal
 
+    attr_accessor :waiter, :customer, :total, :tip
+
+    @@all = [ ]
+
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
 end
